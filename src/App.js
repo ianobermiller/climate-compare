b1f6d2a12591f8ef771640e9d453626a564c39ac
Ian Obermiller <ian@obermillers.com>
2020-12-27 23:41:06 -0800

    Add dropdowns and charts
    Functionally complete now

--- src/App.js
+++ src/App.js
@@ -1,392 +1,147 @@
 import {ResponsiveLine} from '@nivo/line';
+import {useState} from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import {Dropdown} from 'semantic-ui-react';
+import {Container, Dropdown, Form, Header} from 'semantic-ui-react';
 import './App.css';
+import DataSets from './data.json';
 
-const friendOptions = [
-  {
-    key: 'Jenny Hess',
-    text: 'Jenny Hess',
-    value: 'Jenny Hess',
-  },
-  {
-    key: 'Elliot Fu',
-    text: 'Elliot Fu',
-    value: 'Elliot Fu',
-  },
-  {
-    key: 'Stevie Feliciano',
-    text: 'Stevie Feliciano',
-    value: 'Stevie Feliciano',
-  },
-  {
-    key: 'Christian',
-    text: 'Christian',
-    value: 'Christian',
-  },
-  {
-    key: 'Matt',
-    text: 'Matt',
-    value: 'Matt',
-  },
-  {
-    key: 'Justen Kitsune',
-    text: 'Justen Kitsune',
-    value: 'Justen Kitsune',
-  },
-];
+const locations = Object.values(DataSets[0].dataByCityID).sort((a, b) =>
+  a.city.localeCompare(b.city),
+);
 
-const data = [
-  {
-    id: 'japan',
-    color: 'hsl(197, 70%, 50%)',
-    data: [
-      {
-        x: 'plane',
-        y: 70,
-      },
-      {
-        x: 'helicopter',
-        y: 237,
-      },
-      {
-        x: 'boat',
-        y: 275,
-      },
-      {
-        x: 'train',
-        y: 131,
-      },
-      {
-        x: 'subway',
-        y: 133,
-      },
-      {
-        x: 'bus',
-        y: 249,
-      },
-      {
-        x: 'car',
-        y: 10,
-      },
-      {
-        x: 'moto',
-        y: 229,
-      },
-      {
-        x: 'bicycle',
-        y: 155,
-      },
-      {
-        x: 'horse',
-        y: 75,
-      },
-      {
-        x: 'skateboard',
-        y: 5,
-      },
-      {
-        x: 'others',
-        y: 31,
-      },
-    ],
-  },
-  {
-    id: 'france',
-    color: 'hsl(103, 70%, 50%)',
-    data: [
-      {
-        x: 'plane',
-        y: 133,
-      },
-      {
-        x: 'helicopter',
-        y: 216,
-      },
-      {
-        x: 'boat',
-        y: 41,
-      },
-      {
-        x: 'train',
-        y: 112,
-      },
-      {
-        x: 'subway',
-        y: 11,
-      },
-      {
-        x: 'bus',
-        y: 287,
-      },
-      {
-        x: 'car',
-        y: 130,
-      },
-      {
-        x: 'moto',
-        y: 48,
-      },
-      {
-        x: 'bicycle',
-        y: 15,
-      },
-      {
-        x: 'horse',
-        y: 156,
-      },
-      {
-        x: 'skateboard',
-        y: 199,
-      },
-      {
-        x: 'others',
-        y: 17,
-      },
-    ],
-  },
-  {
-    id: 'us',
-    color: 'hsl(22, 70%, 50%)',
-    data: [
-      {
-        x: 'plane',
-        y: 185,
-      },
-      {
-        x: 'helicopter',
-        y: 300,
-      },
-      {
-        x: 'boat',
-        y: 35,
-      },
-      {
-        x: 'train',
-        y: 179,
-      },
-      {
-        x: 'subway',
-        y: 62,
-      },
-      {
-        x: 'bus',
-        y: 256,
-      },
-      {
-        x: 'car',
-        y: 174,
-      },
-      {
-        x: 'moto',
-        y: 130,
-      },
-      {
-        x: 'bicycle',
-        y: 117,
-      },
-      {
-        x: 'horse',
-        y: 258,
-      },
-      {
-        x: 'skateboard',
-        y: 1,
-      },
-      {
-        x: 'others',
-        y: 28,
-      },
-    ],
-  },
-  {
-    id: 'germany',
-    color: 'hsl(52, 70%, 50%)',
-    data: [
-      {
-        x: 'plane',
-        y: 171,
-      },
-      {
-        x: 'helicopter',
-        y: 37,
-      },
-      {
-        x: 'boat',
-        y: 2,
-      },
-      {
-        x: 'train',
-        y: 110,
-      },
-      {
-        x: 'subway',
-        y: 61,
-      },
-      {
-        x: 'bus',
-        y: 261,
-      },
-      {
-        x: 'car',
-        y: 64,
-      },
-      {
-        x: 'moto',
-        y: 18,
-      },
-      {
-        x: 'bicycle',
-        y: 243,
-      },
-      {
-        x: 'horse',
-        y: 154,
-      },
-      {
-        x: 'skateboard',
-        y: 30,
-      },
-      {
-        x: 'others',
-        y: 83,
-      },
-    ],
-  },
-  {
-    id: 'norway',
-    color: 'hsl(52, 70%, 50%)',
-    data: [
-      {
-        x: 'plane',
-        y: 129,
-      },
-      {
-        x: 'helicopter',
-        y: 48,
-      },
-      {
-        x: 'boat',
-        y: 55,
-      },
-      {
-        x: 'train',
-        y: 142,
-      },
-      {
-        x: 'subway',
-        y: 72,
-      },
-      {
-        x: 'bus',
-        y: 272,
-      },
-      {
-        x: 'car',
-        y: 210,
-      },
-      {
-        x: 'moto',
-        y: 253,
-      },
-      {
-        x: 'bicycle',
-        y: 44,
-      },
-      {
-        x: 'horse',
-        y: 3,
-      },
-      {
-        x: 'skateboard',
-        y: 183,
-      },
-      {
-        x: 'others',
-        y: 227,
-      },
-    ],
-  },
+const locationOptions = locations.map(loc => ({
+  key: loc.id,
+  text: loc.city + ', ' + loc.state,
+  value: loc.id,
+}));
+
+const dataSetOptions = DataSets.map(ds => ({
+  key: ds.name,
+  text: ds.name,
+  value: ds.name,
+}));
+
+const monthNames = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
 ];
 
 export default function App() {
+  const [locations, setLocations] = useState(['13959', '13891', '14839']);
+  const [dataSetNames, setDataSetNames] = useState(
+    dataSetOptions.map(o => o.value),
+  );
+
+  const selectedDataSets = DataSets.filter(ds =>
+    dataSetNames.includes(ds.name),
+  );
+  const charts = selectedDataSets.map(ds => ({
+    name: ds.name,
+    data: locations
+      .filter(loc => ds.dataByCityID[loc])
+      .map(loc => ({
+        id: ds.dataByCityID[loc].city,
+        data: ds.dataByCityID[loc].valueByMonth.map((v, i) => ({
+          x: monthNames[i],
+          y: v,
+        })),
+      })),
+  }));
+
   return (
-    <div className="App">
-      <Dropdown
-        placeholder="Select Friend"
-        fluid
-        selection
-        options={friendOptions}
-      />
-      <div style={{height: 400}}>
-        <ResponsiveLine
-          data={data}
-          margin={{top: 50, right: 110, bottom: 50, left: 60}}
-          xScale={{type: 'point'}}
-          yScale={{
-            type: 'linear',
-            min: 'auto',
-            max: 'auto',
-            stacked: true,
-            reverse: false,
-          }}
-          yFormat=" >-.2f"
-          curve="catmullRom"
-          axisTop={null}
-          axisRight={null}
-          axisBottom={{
-            orient: 'bottom',
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'transportation',
-            legendOffset: 36,
-            legendPosition: 'middle',
-          }}
-          axisLeft={{
-            orient: 'left',
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'count',
-            legendOffset: -40,
-            legendPosition: 'middle',
-          }}
-          colors={{scheme: 'category10'}}
-          pointSize={10}
-          pointColor={{theme: 'background'}}
-          pointBorderWidth={2}
-          pointBorderColor={{from: 'serieColor'}}
-          pointLabelYOffset={-12}
-          useMesh={true}
-          legends={[
-            {
-              anchor: 'bottom-right',
-              direction: 'column',
-              justify: false,
-              translateX: 100,
-              translateY: 0,
-              itemsSpacing: 0,
-              itemDirection: 'left-to-right',
-              itemWidth: 80,
-              itemHeight: 20,
-              itemOpacity: 0.75,
-              symbolSize: 12,
-              symbolShape: 'circle',
-              symbolBorderColor: 'rgba(0, 0, 0, .5)',
-              effects: [
-                {
-                  on: 'hover',
-                  style: {
-                    itemBackground: 'rgba(0, 0, 0, .03)',
-                    itemOpacity: 1,
+    <Container className="App" text={true}>
+      <Header as="h1">Compare Climates of U.S. Cities</Header>
+      <Form>
+        <Form.Field>
+          <label>Locations</label>
+          <Dropdown
+            fluid={true}
+            multiple={true}
+            onChange={(e, {value}) => setLocations(value)}
+            options={locationOptions}
+            placeholder="Select Locations"
+            search={true}
+            selection={true}
+            value={locations}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Data</label>
+          <Dropdown
+            fluid={true}
+            multiple={true}
+            onChange={(e, {value}) => setDataSetNames(value)}
+            options={dataSetOptions}
+            placeholder="Select Data Sets"
+            search={true}
+            selection={true}
+            value={dataSetNames}
+          />
+        </Form.Field>
+        {charts.map(chart => (
+          <Form.Field key={chart.name}>
+            <label>{chart.name}</label>
+            <div style={{height: 400}}>
+              <ResponsiveLine
+                data={chart.data}
+                margin={{top: 20, right: 20, bottom: 50, left: 30}}
+                xScale={{type: 'point'}}
+                yScale={{
+                  type: 'linear',
+                  // min: 'auto',
+                  max: 'auto',
+                  stacked: false,
+                  reverse: false,
+                }}
+                curve="catmullRom"
+                colors={{scheme: 'category10'}}
+                pointSize={5}
+                pointBorderWidth={2}
+                pointLabelYOffset={-12}
+                useMesh={true}
+                // enablePointLabel={true}
+                enableSlices="x"
+                legends={[
+                  {
+                    anchor: 'bottom',
+                    direction: 'row',
+                    justify: false,
+                    translateX: 0,
+                    translateY: 50,
+                    itemsSpacing: 0,
+                    itemDirection: 'left-to-right',
+                    itemWidth: 120,
+                    itemHeight: 20,
+                    itemOpacity: 0.75,
+                    symbolSize: 12,
+                    symbolShape: 'circle',
+                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                    effects: [
+                      {
+                        on: 'hover',
+                        style: {
+                          itemBackground: 'rgba(0, 0, 0, .03)',
+                          itemOpacity: 1,
+                        },
+                      },
+                    ],
                   },
-                },
-              ],
-            },
-          ]}
-        />
-      </div>
-    </div>
+                ]}
+              />
+            </div>
+          </Form.Field>
+        ))}
+      </Form>
+    </Container>
   );
 }
